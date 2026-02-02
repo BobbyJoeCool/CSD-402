24d6065699ddb7ab5dc18592c2e1d65e0af6a38e
Robert Breutzmann <robert.breutzmann@gmail.com>
2026-02-02 15:04:14 -0600

    Upload Discussion and Assignments

---
+++ module-5/Exercises/ConnectFour.java
@@ -0,0 +1,9 @@
+/* 
+Connect four is a two-player board game in which the players alternately drop colored disks into a seven-column, six-row vertically suspended grid.
+
+The objective of the game is to connect four same-colored disks in a row, a column, or a diagonal before your opponent can do likewise. The program prompts two players to drop a red or yellow disk alternately. Whenever a disk is dropped, the program displays the re-board on the console and determines the status of the game (win, draw, or continue).
+*/
+
+public class ConnectFour {
+    
+}
