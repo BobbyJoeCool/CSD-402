4c66a12e9ba7efc789f2a1d011ffd182244f8e39
Robert Breutzmann <robert.breutzmann@gmail.com>
2026-02-03 22:06:15 -0600

    Upload Exercises

--- module-5/Exercises/LatinSquare.java
+++
@@ -1,9 +0,0 @@
-/* 
-A Latin square is an n-by-n array filled with n different Latin letters, each occurring exactly once in each row and once in each column. Write a program that prompts the user to enter the number n and the array of characters, as shown in the sample output, and checks if the input array is a Latin square. The characters are the first n characters starting from A.
-*/
-
-public class LatinSquare {
-    public static void main(String[] args) {
-        
-    }
-}
