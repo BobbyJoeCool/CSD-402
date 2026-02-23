c3d6f83fe84bd669e0e7a61ac47a67cb5828f74e
Robert Breutzmann <robert.breutzmann@gmail.com>
2026-02-23 10:23:26 -0600

    Upload Program Shells

---
+++ module-9/FileIOProgram.java
@@ -0,0 +1,14 @@
+/* 
+    Breutzmann, Robert
+    CSD 402 - Java for Programmers
+    Assignment 9.2
+    Due Date: March 1st, 2026
+ */
+
+/*
+Write a program to create a new file titled data.file, if the file does not exist. Then write to the new file, adding 10 randomly generated numbers, or append 10 randomly generated numbers to a previous file. Each integer is to be separated by a space. Close the file, then reopen the file, read the data from the file, and display it.
+*/
+
+public class FileIOProgram {
+
+}
---
+++ module-9/TryExceptProgram.java
@@ -0,0 +1,14 @@
+/* 
+    Breutzmann, Robert
+    CSD 402 - Java for Programmers
+    Assignment 9.2
+    Due Date: March 1st, 2026
+ */
+
+/*
+Write a program that uses an ArrayList filled with a minimum of 10 Strings. Use a ‘for-each’ loop to print the ArrayList collection. Then ask a user which element they would like to see again. Then, attempt to print the element in a try/catch format, which will result in the element being displayed. If the element value received is invalid, display a message that an Exception has been thrown, displaying “Out of Bounds”. In this program, include the use of Autoboxing/Auto-Unboxing, working with a user's String input.
+*/
+
+public class TryExceptProgram {
+
+}
