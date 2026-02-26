4c9c94ce01375e6197af430171ba03e4ba5fc5a2
Robert Breutzmann <robert.breutzmann@gmail.com>
2026-02-25 22:51:17 -0600

    Upload Module 9 Assignments

--- module-9/FileIOProgram.java
+++ module-9/FileIOProgram.java
@@ -5,10 +5,63 @@
     Due Date: March 1st, 2026
  */
 
-/*
-Write a program to create a new file titled data.file, if the file does not exist. Then write to the new file, adding 10 randomly generated numbers, or append 10 randomly generated numbers to a previous file. Each integer is to be separated by a space. Close the file, then reopen the file, read the data from the file, and display it.
-*/
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 
 public class FileIOProgram {
+    public static void main(String[] args) {
+        //create a new file titled data.file, if the file does not exist.
+        File file = new File("data.file");
 
+        try {
+            file.createNewFile();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        // Creates an Array with 10 random numbers.
+        Random rand = new Random();
+        List<Integer> numbers = new ArrayList<>(10);
+        for (int i = 0; i < 10; i++) {
+            numbers.add(rand.nextInt(1000));
+        }
+
+        /*
+        Then write to the new file, adding 10 randomly generated numbers
+        or append 10 randomly generated numbers to a previous file.
+        Each integer is to be separated by a space.
+        Close the file (accomplished using "try-with resources")
+        */
+        try (FileWriter writer = new FileWriter(file, true)) {
+            if (file.length() > 0) {
+                writer.write(System.lineSeparator()); // This line starts the appending process of numbers on a new line.
+            }
+            for (Integer integer : numbers) {
+                writer.write(integer + " ");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } // File automatically closes when try block completes.
+
+        // reopen the file, read the data, and display it.
+        try (Scanner reader = new Scanner(file)) {
+            int count = 0;
+            while (reader.hasNextInt()) {
+                System.out.print(reader.nextInt() + " ");
+                count++;
+                // Starts a new line every 10 numbers.
+                if (count % 10 == 0) {
+                    System.out.println();
+                }
+            }
+            System.out.println();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }   
 }
--- module-9/TryExceptProgram.java
+++ module-9/TryExceptProgram.java
@@ -5,10 +5,58 @@
     Due Date: March 1st, 2026
  */
 
-/*
-Write a program that uses an ArrayList filled with a minimum of 10 Strings. Use a ‘for-each’ loop to print the ArrayList collection. Then ask a user which element they would like to see again. Then, attempt to print the element in a try/catch format, which will result in the element being displayed. If the element value received is invalid, display a message that an Exception has been thrown, displaying “Out of Bounds”. In this program, include the use of Autoboxing/Auto-Unboxing, working with a user's String input.
-*/
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 public class TryExceptProgram {
+    public static void main(String[] args) {
 
+        //uses an ArrayList filled with a minimum of 10 Strings.
+        ArrayList<String> countries = new ArrayList<>(
+            Arrays.asList(
+                "United States",
+                "Canada",
+                "Mexico",
+                "Brazil",
+                "United Kingdom",
+                "France",
+                "Germany",
+                "India",
+                "China",
+                "Japan"
+            )
+        );
+
+        // Use a ‘for-each’ loop to print the ArrayList collection.
+        for (String countryString : countries) {
+            System.out.println(countryString);
+        }
+
+        Scanner input = new Scanner(System.in);
+
+        while (true) { //While loop to try again until successful input.
+            // Then ask a user which element they would like to see again. 
+            System.out.print("Which element would you like to see again (enter an integer)? ");
+            String requestedIndex = input.nextLine();
+
+            /*
+            Then, attempt to print the element in a try/catch format, which will result in the element being displayed. 
+            If the element value received is invalid, display a message that an Exception has been thrown, displaying “Out of Bounds”.
+            */
+            try {
+                //include the use of Autoboxing/Auto-Unboxing, working with a user's String input
+                Integer requestedIndexInt = Integer.parseInt(requestedIndex);
+                String requestedString = countries.get(requestedIndexInt);
+                System.out.println(requestedString);
+                break;
+            } catch (NumberFormatException ex) {
+                System.out.println("Exception Thrown: Entry not a valid integer.");
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println("Exception Thrown: Index Out of Bounds");
+            }
+
+        }
+
+        input.close();
+    }
 }
